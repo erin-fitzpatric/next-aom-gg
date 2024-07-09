8ee357d886aaef6797b0c7c9f20f5d9f8b70ae77
Erin Fitzpatric <efitzpatric@gmail.com>
2024-07-08 22:35:55 -0400

    updated table footer stlying

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         >
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
-            enableSystem
+            defaultTheme="dark"
             disableTransitionOnChange
           >
             {children}
