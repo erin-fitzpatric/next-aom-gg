dd34237d1a7bd84ba057106b6f3e757b0e402ab4
Logg-y <49533518+Logg-y@users.noreply.github.com>
2024-07-22 01:16:36 +0100

    Mapped object for major god info

--- src/types/MajorGods.ts
+++ src/types/MajorGods.ts
@@ -1,29 +1,76 @@
-export enum MajorGods {
-  Zeus = 1, 
-  Hades = 2,
-  Poseidon = 3,
-  Ra = 4,
-  Isis = 5,
-  Set = 6,
-  Thor = 7,
-  Odin = 8,
-  Loki = 9,
-  Kronos = 10,
-  Oranos = 11,
-  Gaia = 12,
+
+interface MajorGodData
+{
+    name: string,
+    portraitPath: string,
 }
 
-export const MajorGodsPortraitPath: { [key: string]: string } = {
-  Zeus: "/gods/greeks/major-gods/zeus_icon.png",
-  Poseidon: "/gods/greeks/major-gods/poseidon_icon.png",
-  Hades: "/gods/greeks/major-gods/hades_icon.png",
-  Isis: "/gods/egyptians/major-gods/isis_icon.png",
-  Ra: "/gods/egyptians/major-gods/ra_icon.png",
-  Set: "/gods/egyptians/major-gods/set_icon.png",
-  Odin: "/gods/norse/major-gods/odin_icon.png",
-  Thor: "/gods/norse/major-gods/thor_icon.png",
-  Loki: "/gods/norse/major-gods/loki_icon.png",
-  Oranos: "/gods/atlanteans/major-gods/oranos_icon.png",
-  Kronos: "/gods/atlanteans/major-gods/kronos_icon.png",
-  Gaia: "/gods/atlanteans/major-gods/gaia_icon.png",
-};
+const UNKNOWN_PORTRAIT_PATH =  "/gods/greeks/major-gods/zeus_icon.png"; // TODO use the random ? icon
+
+const MajorGodsByIndex = new Map<number, MajorGodData>([
+    [0, {
+        name: "Nature",
+        portraitPath: UNKNOWN_PORTRAIT_PATH,
+    }],
+    [1, {
+        name: "Zeus",
+        portraitPath: "/gods/greeks/major-gods/zeus_icon.png",
+    }],
+    [2, {
+        name: "Hades",
+        portraitPath: "/gods/greeks/major-gods/hades_icon.png",
+    }],
+    [3, {
+        name: "Poseidon",
+        portraitPath: "/gods/greeks/major-gods/poseidon_icon.png",
+    }],
+    [4, {
+        name: "Ra",
+        portraitPath: "/gods/egyptians/major-gods/ra_icon.png",
+    }],
+    [5, {
+        name: "Isis",
+        portraitPath: "/gods/egyptians/major-gods/isis_icon.png",
+    }],
+    [6, {
+        name: "Set",
+        portraitPath: "/gods/egyptians/major-gods/set_icon.png",
+    }],
+    [7, {
+        name: "Thor",
+        portraitPath: "/gods/norse/major-gods/thor_icon.png",
+    }],
+    [8, {
+        name: "Odin",
+        portraitPath: "/gods/norse/major-gods/odin_icon.png",
+    }],
+    [9, {
+        name: "Loki",
+        portraitPath: "/gods/norse/major-gods/loki_icon.png",
+    }],
+    [10, {
+        name: "Kronos",
+        portraitPath: "/gods/atlantean/major-gods/kronos_icon.png",
+    }],
+    [11, {
+        name: "Oranos",
+        portraitPath: "/gods/atlantean/major-gods/oranos_icon.png",
+    }],
+    [12, {
+        name: "Gaia",
+        portraitPath: "/gods/atlantean/major-gods/gaia_icon.png",
+    }],
+]);
+
+export function majorGodIndexToData(index: number): MajorGodData
+{
+    let data = MajorGodsByIndex.get(index);
+    if (data === undefined)
+    {
+        return {
+            name: `Unknown ${index}`,
+            portraitPath: UNKNOWN_PORTRAIT_PATH,
+        }
+    }
+    return data;
+}
