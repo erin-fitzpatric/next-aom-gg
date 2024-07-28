8f662fb45301a234faec1b9f940647ab723980a8
Erin Fitzpatric <efitzpatric@gmail.com>
2024-07-28 15:10:18 -0400

    more timezone fixes

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,55 +1,57 @@
-"use client";
+'use client';
 import React, { useState, useEffect } from "react";
 
-export default function Countdown({
-  targetDate,
-  title,
-}: {
+interface CountdownProps {
   targetDate: Date;
   title: string;
-}) {
-  const [timeLeft, setTimeLeft] = useState({
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  });
+  timezoneOffset: number; // timezone offset in minutes
+}
+
+const Countdown: React.FC<CountdownProps> = ({ targetDate, title, timezoneOffset }) => {
+  const [timeLeft, setTimeLeft] = useState<{
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  } | null>(calculateTimeLeft(targetDate, timezoneOffset));
 
   useEffect(() => {
-    function calculateTimeLeft() {
-      const difference = +new Date(targetDate) - +new Date();
-      let timeLeft = {
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      };
-
-      if (difference > 0) {
-        timeLeft = {
-          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-          minutes: Math.floor((difference / 1000 / 60) % 60),
-          seconds: Math.floor((difference / 1000) % 60),
-        };
-      }
-
-      return timeLeft;
-    }
-    setTimeLeft(calculateTimeLeft());
     const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
+      setTimeLeft(calculateTimeLeft(targetDate, timezoneOffset));
     }, 1000);
+
     return () => clearInterval(timer);
-  }, [targetDate]);
+  }, [targetDate, timezoneOffset]);
+
+  function calculateTimeLeft(targetDate: Date, timezoneOffset: number) {
+    const adjustedTargetDate = new Date(targetDate.getTime() - timezoneOffset * 60000);
+    const difference = +adjustedTargetDate - +new Date();
+    let timeLeft: { days: number; hours: number; minutes: number; seconds: number } | null = null;
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  }
 
   return (
-    <div className="text-gold text-center mr-10 font-bold">
-      <h1>{title}</h1>
-      <div>
-        {timeLeft.days}d {timeLeft.hours}h {timeLeft.minutes}m{" "}
-        {timeLeft.seconds}s
-      </div>
+    <div className="countdown">
+      <h3>{title}</h3>
+      {timeLeft ? (
+        <div>
+          {timeLeft.days}d {timeLeft.hours}h {timeLeft.minutes}m {timeLeft.seconds}s
+        </div>
+      ) : (
+        <div>Time is up!</div>
+      )}
     </div>
   );
-}
+};
+
+export default Countdown;
