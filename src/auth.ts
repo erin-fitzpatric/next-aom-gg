02d0644b4a10ce5c412ba5d489fa483c877ce3f3
Erin Fitzpatric <efitzpatric@gmail.com>
2024-08-09 02:08:38 -0400

    Added auth for xbox and steam

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,26 @@
 import NextAuth from "next-auth";
-import Discord from "next-auth/providers/discord";
-import Twitch from "next-auth/providers/twitch";
-// TODO - add steam
-// TODO - add xbox
+// import Discord from "next-auth/providers/discord";
+// import Twitch from "next-auth/providers/twitch";
+import Steam from "steam-next-auth"
+import Xbox from "./server/providers/xbox";
 
-// TODO
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import { MongoClient } from "mongodb";
 
 const mongoClient = new MongoClient(process.env.MONGODB_URI as string, {});
-console.log('doing auth things')
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  adapter: MongoDBAdapter(mongoClient),
-  providers: [Discord, Twitch],
+
+export const { auth, handlers, signIn, signOut } = NextAuth((req) => {
+  return {
+    adapter: MongoDBAdapter(mongoClient),
+    providers: [
+      // Discord,
+      // Twitch,
+      Xbox,
+      ...(req ? [Steam(req, {
+        clientSecret: process.env.AUTH_STEAM_KEY!,
+        callbackUrl: `${process.env.BASE_URL}/api/openid`
+      })] : []),
+    ],
+  }
+
 });
--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -6,6 +6,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { DialogDescription } from "@radix-ui/react-dialog";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 
@@ -23,18 +24,23 @@ export function SignIn() {
   return (
     <>
       {userName && userImage ? (
-       <div className="relative flex flex-col items-center">
-       <Image
-         src={userImage}
-         width={64}
-         height={64}
-         alt="User"
-         className="mx-auto cursor-pointer filter transition-transform duration-300 ease-in-out"
-       />
-       <div onClick={() => signOut()} className="absolute cursor-pointer inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out">
-         <span className="text-secondary-foreground text-lg font-semibold">Log out</span>
-       </div>
-     </div>
+        <div className="relative flex flex-col items-center">
+          <Image
+            src={userImage}
+            width={64}
+            height={64}
+            alt="User"
+            className="mx-auto cursor-pointer filter transition-transform duration-300 ease-in-out"
+          />
+          <div
+            onClick={() => signOut()}
+            className="absolute cursor-pointer inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out"
+          >
+            <span className="text-secondary-foreground text-lg font-semibold">
+              Log out
+            </span>
+          </div>
+        </div>
       ) : (
         <Dialog>
           <DialogTrigger asChild>
@@ -46,16 +52,19 @@ export function SignIn() {
             </Button>
           </DialogTrigger>
           <DialogContent
-            className="sm:max-w-[275px]"
+            className="sm:max-w-[300px]"
             onClick={handleSignIn}
             aria-describedby="sign-in-modal"
           >
             <DialogHeader>
-              <DialogTitle id="sign-in-modal" className="mx-auto text-pr">
+              <DialogTitle id="sign-in-modal" className="mx-auto text-pr text-gold font-bold">
                 Sign In
               </DialogTitle>
             </DialogHeader>
-            <div className="flex w-full">
+            <DialogDescription className="mx-auto text-center">
+              Sign in to AoM.gg to rate, upload, and download games.
+            </DialogDescription>
+            {/* <div className="flex w-full">
               <div className="mx-auto gap-1">
                 <Button
                   variant="secondary"
@@ -92,6 +101,38 @@ export function SignIn() {
                   Sign in with Discord
                 </Button>
               </div>
+            </div> */}
+            <div className="flex w-full flex-col items-center gap-2">
+              <Button
+                variant="secondary"
+                type="submit"
+                className="w-full max-w-xs mx-auto flex items-center justify-center"
+                value={"steam"}
+              >
+                <Image
+                  src="/login/steam.svg"
+                  width={35}
+                  height={35}
+                  alt="Steam"
+                  className="mr-2 ml-2"
+                />
+                Sign in with Steam
+              </Button>
+              <Button
+                variant="secondary"
+                type="submit"
+                className="w-full max-w-xs mx-auto flex items-center justify-center"
+                value={"xbox"}
+              >
+                <Image
+                  src="/login/xbox.png"
+                  width={40}
+                  height={40}
+                  alt="Microsoft"
+                  className="mr-2"
+                />
+                Sign in with Xbox
+              </Button>
             </div>
           </DialogContent>
         </Dialog>
