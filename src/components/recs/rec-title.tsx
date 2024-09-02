2ecad83d7503fde6b99f6456e0534b0ed05bd558
seanspade <skspade@pm.me>
2024-09-02 12:06:31 -0400

    Add optional className prop to RecTitle component

--- src/components/recs/rec-title.tsx
+++ src/components/recs/rec-title.tsx
@@ -1,6 +1,14 @@
-export default function RecTitle({gameTitle}: {gameTitle: string}) {
+export default function RecTitle({
+  gameTitle,
+  className = "",
+}: {
+  gameTitle: string;
+  className?: string;
+}) {
   return (
-    <div className="text-center text-xl text-prim font-semibold w-[240px] min-h-2-lines line-clamp-2">
+    <div
+      className={`text-center text-xl text-prim font-semibold w-[240px] min-h-2-lines line-clamp-2 ${className}`}
+    >
       {gameTitle}
     </div>
   );
