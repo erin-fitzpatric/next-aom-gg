16f0abc240a117fa4218a136c5baf23fe1562171
seanspade <skspade@pm.me>
2024-09-02 11:41:59 -0400

    Add TopRecordedGames component

---
+++ src/components/recs/top-recorded-games.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { SpinnerWithText } from "../spinner";
+import { Card } from "../ui/card";
+import RecTile from "./rec-tile";
+import { getMythRecs } from "@/server/controllers/mongo-controller";
+import Loading from "../loading";
+import { useQuery } from "@/utils/useQuery";
+import { useCallback } from "react";
+
+export default function TopRecordedGames() {
+  // Set state
+
+  const getMythRecsCallback = useCallback(() => getMythRecs(0), []);
+  const [{ data: recs, loading: isLoading }, isInitialFetch] = useQuery(
+    ["getMythRecs"],
+    getMythRecsCallback,
+  );
+
+  if (isInitialFetch && isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="relative">
+      {recs && recs.length === 0 && !isInitialFetch ? (
+        <div className="flex justify-center mt-4">
+          <Card className="p-4 w-full">
+            <p className="flex justify-center">No recorded games found!</p>
+          </Card>
+        </div>
+      ) : (
+        <Card className="p-4">
+          {/* Replay Gallery */}
+          <div>
+            <div className="flex flex-row flex-wrap justify-center">
+              {recs?.map((rec) => (
+                <Card
+                  key={rec.gameGuid}
+                  className="bg-secondary rounded-lg m-1 p-2 flex w-fit"
+                >
+                  <div>
+                    <RecTile
+                      showMap={false}
+                      key={`rec-tile-${rec.gameGuid}`}
+                      rec={rec}
+                    ></RecTile>
+                  </div>
+                </Card>
+              ))}
+            </div>
+            {isLoading && (
+              <div className="flex justify-center mt-4">
+                <SpinnerWithText text={"Loading recorded games..."} />
+              </div>
+            )}
+          </div>
+        </Card>
+      )}
+    </div>
+  );
+}
