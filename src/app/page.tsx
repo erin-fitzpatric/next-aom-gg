3f18f163b624f2ee272c598f9117c701433c84ed
seanspade <skspade@pm.me>
2024-09-09 23:14:17 -0400

    Update grid layout for main page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,14 +17,14 @@ export default function Home() {
       <div>
         <RedditFeed />
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-5 gap-5">
-        <div className="md:col-span-1 lg:col-span-1 order-2 sm:order-2 xl:order-1">
+      <div className="grid grid-cols-1 xl:grid-cols-4 gap-5">
+        <div className="xl:col-span-1 order-2 xl:order-1">
           <TopRecordedGames />
         </div>
-        <div className="md:col-span-2 lg:col-span-3 order-1 sm:order-1 xl:order-2">
+        <div className="xl:col-span-2 order-1 xl:order-2">
           <Leaderboard />
         </div>
-        <div className="md:col-span-1 lg:col-span-1 order-3 sm:order-3 xl:order-3">
+        <div className="xl:col-span-1 order-3">
           <FeaturedYoutubeVideos />
         </div>
       </div>
