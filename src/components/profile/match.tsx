fec8b2625c65f5aa999136d5aeef73a0e22bf320
Sean <43147936+skspade@users.noreply.github.com>
2024-09-21 11:40:36 -0400

    fix: break match history to mobile on larger screens

--- src/components/profile/match.tsx
+++ src/components/profile/match.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import RatingChange from "./ratingChange";
 import Team from "./team";
-import { Match as MatchType} from "@/types/Match";
+import { Match as MatchType } from "@/types/Match";
 
 type IProps = {
   match: MatchType;
@@ -28,10 +28,9 @@ export default function MatchComponent({ match }: IProps) {
     router.push(route);
   };
 
-
   return (
-    <div className="p-4 border rounded-md shadow-lg flex flex-col md:flex-row md:space-x-4">
-      <div className="flex flex-col items-center md:items-start justify-center flex-shrink-0 h-full">
+    <div className="p-4 border rounded-md shadow-lg flex flex-col xl:flex-row xl:space-x-4">
+      <div className="flex flex-col items-center xl:items-start justify-center flex-shrink-0 h-full">
         <div className="text-xl font-semibold text-center mb-2">
           {new Date(matchDate).toLocaleDateString()}
         </div>
@@ -43,21 +42,21 @@ export default function MatchComponent({ match }: IProps) {
               alt={mapData.name}
               width={164}
               height={164}
-              className="md:block hidden"
+              className="xl:block hidden"
             />
           </div>
         )}
       </div>
 
       {/* Details */}
-      <div className="flex flex-col justify-center space-y-2 flex-grow text-center mx-auto md:text-left md:mx-0">
+      <div className="flex flex-col justify-center space-y-2 flex-grow text-center mx-auto xl:text-left xl:mx-0">
         <div>{gameMode}</div>
         <div>{matchType}</div>
         <div>{mapData.name}</div>
         <div>{formatTime(Math.floor(matchDuration))}</div>
       </div>
 
-      <div className="flex flex-col items-center md:items-end flex-shrink space-y-4">
+      <div className="flex flex-col items-center xl:items-end flex-shrink space-y-4">
         {/* Rating Change */}
         <RatingChange
           ratingChange={ratingChange}
