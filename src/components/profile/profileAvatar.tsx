4369cbc5bb0a1b31f84057102a8dde5038a36ad6
Sean <43147936+skspade@users.noreply.github.com>
2024-09-22 19:57:39 -0400

    committing before major refactor

--- src/components/profile/profileAvatar.tsx
+++ src/components/profile/profileAvatar.tsx
@@ -13,17 +13,17 @@ export function ProfileAvatar({
   return (
     <>
       {loading ? (
-        <Skeleton className="w-24 h-24 rounded-full mx-auto" />
+        <Skeleton className="w-[84px] h-[84px] rounded-full" />
       ) : steamProfile ? (
         <Image
           src={steamProfile.avatarfull}
           alt="Profile Picture"
           width={84}
           height={84}
-          className="rounded-md mx-auto"
+          className="rounded-md w-[84px] h-[84px] shrink-0"
         />
       ) : (
-        <div className="w-24 h-24 rounded-full mx-auto bg-gray-300"></div>
+        <div className="w-[84px] h-[84px] rounded-full mx-auto bg-gray-300"></div>
       )}
     </>
   );
