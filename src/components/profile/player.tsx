103a7e8d4bf416a21070d080909226a3ed32bc40
Sean <43147936+skspade@users.noreply.github.com>
2024-09-21 22:15:27 -0400

    more tune up

--- src/components/profile/player.tsx
+++ src/components/profile/player.tsx
@@ -1,65 +1,53 @@
 import { MappedTeam } from "@/app/api/matchHistory/matchHelpers";
 import { majorGodIndexToData } from "@/types/MajorGods";
-import { TeamResult, MatchHistoryMap } from "@/types/MatchHistory";
+import { MatchHistoryMap } from "@/types/MatchHistory";
 import Image from "next/image";
 
-interface Iprops {
-  team: TeamResult;
+interface IProps {
+  player: MappedTeam;
   matchHistoryMap: MatchHistoryMap;
   handleNameClick: (row: number) => void;
   gameMode: string;
 }
 
 export default function Player({
-  team,
+  player,
   matchHistoryMap,
   handleNameClick,
   gameMode,
-}: Iprops) {
+}: IProps) {
   const isCustom = gameMode === "CUSTOM";
+  const { portraitPath, name: civName } = majorGodIndexToData(
+    Number(player.civilization_id),
+  );
+  const isWinner = player.resulttype === 1;
+  const ratingChangeColor = isWinner ? "primary" : "red-500";
 
   return (
-    <>
-      {team.results.map((player: MappedTeam) => {
-        const { portraitPath, name: civName } = majorGodIndexToData(
-          Number(player.civilization_id),
-        );
-        const isWinner = player.resulttype === 1;
-        const ratingChangeColor = isWinner
-          ? "border-primary"
-          : "border-red-500";
-
-        return (
-          <div
-            key={player.profile_id}
-            className="flex flex-col gap-1 shadow-card md:flex-row items-center w-full"
-          >
-            {/* Civ Image */}
-            <Image
-              src={portraitPath}
-              alt={civName}
-              width={24}
-              height={24}
-              className={`rounded-full border-2 ${ratingChangeColor}`}
-            />
-            {/* Player Name */}
-            <div
-              className="text-base text-left cursor-pointer flex-1 mb-2 md:mb-0 overflow-hidden truncate mr-2"
-              onClick={() => handleNameClick(player.profile_id)}
-              title={player.playerName}
-            >
-              {player.playerName}
-            </div>
-
-            {/* Rating */}
-            {!isCustom && (
-              <div className="text-small font-light text-right">
-                {matchHistoryMap[player.profile_id]?.[0]?.newrating}
-              </div>
-            )}
+    <div className="grid grid-cols-[1fr,4fr] gap-x-1 items-center w-full">
+      {/* Civ Image */}
+      <Image
+        src={portraitPath}
+        alt={civName}
+        width={24}
+        height={24}
+        className={`shrink-0 rounded-full border-2 w-[24px] h-6 border-${ratingChangeColor}`}
+      />
+      {/* Player Name and Rating */}
+      <div className="flex justify-between items-center">
+        <div
+          className={`text-base cursor-pointer overflow-hidden truncate text-${ratingChangeColor}`}
+          onClick={() => handleNameClick(player.profile_id)}
+          title={player.playerName}
+        >
+          {player.playerName}
+        </div>
+        {!isCustom && (
+          <div className="text-xs font-light">
+            {matchHistoryMap[player.profile_id]?.[0]?.newrating}
           </div>
-        );
-      })}
-    </>
+        )}
+      </div>
+    </div>
   );
 }
