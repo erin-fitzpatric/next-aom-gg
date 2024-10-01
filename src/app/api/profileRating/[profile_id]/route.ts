8a13a1bf7f9a2e6b992f28c22453141f40e69ba3
Prateek Singh <prateekvns99@gmail.com>
2024-10-01 11:43:56 +0530

    added type

--- src/app/api/profileRating/[profile_id]/route.ts
+++ src/app/api/profileRating/[profile_id]/route.ts
@@ -1,6 +1,5 @@
 import { MatchModel } from "@/db/mongo/model/MatchModel";
 import getMongoClient from "@/db/mongo/mongo-client";
-import { MatchHistoryMember } from "@/types/MatchHistory";
 
 export async function GET(req: Request) {
   const url = new URL(req.url);
@@ -8,43 +7,35 @@ export async function GET(req: Request) {
   const profileId = Number(pathSegments[pathSegments.length - 1]);
   await getMongoClient();
 
-  console.log("Profile ID:", profileId);
-
   try {
     const matches = await MatchModel.find({
       gameMode: "1V1_SUPREMACY",
       [`matchHistoryMap.${profileId}`]: { $exists: true },
     });
-    if (matches.length > 0) {
-      const allNewRatings: any = [];
 
+    if (matches.length > 0) {
+      const allNewRatings: number[] = [];
       matches.forEach((match) => {
         const matchHistoryMapData = match.matchHistoryMap;
-        // Convert matchHistoryMapData into an array of arrays
-        const arrayOfArrays = Array.from(matchHistoryMapData).map(
-          ([key, value]) => [key, ...value]
-        );
 
-        // Find the profile data for the given profileId
-        const profileData = arrayOfArrays.find(
-          (arr) => Number(arr[0]) === profileId
-        );
-
-        // If profile data exists, push the newRating to the array
-        if (profileData) {
-          const matchHistoryEntry = profileData[1] as MatchHistoryMember; // Type assertion if needed
-          allNewRatings.push(matchHistoryEntry.newrating); // Collecting newRating
+        if (matchHistoryMapData instanceof Map) {
+          matchHistoryMapData.forEach((matchHistoryEntries, key) => {
+            if (Number(key) === profileId) {
+              matchHistoryEntries.forEach((entry: any) => {
+                allNewRatings.push(entry.newrating);
+              });
+            }
+          });
+        } else {
+          throw new Error("Invalid matchHistoryMapData format");
         }
       });
-
-      console.log(allNewRatings);
+      return Response.json(allNewRatings);
     } else {
-      console.log(`No data found for profile ID: ${profileId}`);
+      throw new Error(`No data found for profile ID: ${profileId}`);
     }
-
-    return Response.json({ Message: "Hi" });
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error fetching matches:", error);
-    return Response.json({ error: "Failed to fetch data" }, { status: 500 });
+    return Response.json({ error: error.message }, { status: 500 });
   }
 }
